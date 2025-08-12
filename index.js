91685a4450c133f6dfc2d3ba0e8376cedb853ce8
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-08-12 09:04:45 +0200

    add database

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const nameInput = document.getElementById("name-input")
-const emailInput = document.getElementById("email-input").value 
+// DATABASE SET UP - Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBlvO36aDJ3VpD86R_mogHskew3IsMcPaM",
+    authDomain: "ecyd-registrationform.firebaseapp.com",
+    projectId: "ecyd-registrationform",
+    storageBucket: "ecyd-registrationform.firebasestorage.app",
+    messagingSenderId: "991779697333",
+    appId: "1:991779697333:web:b9387f813d2d8bd987c4fc"
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+// ----------------------------------------------------------------------
+
 const form = document.querySelector("form")
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async (e) => {
     e.preventDefault()
-    console.log("prevented")
+
+    const nameInputValue = document.getElementById("name-input").value.trim()
+    const emailInputValue = document.getElementById("email-input").value.trim()
+
+    try {
+        await addDoc(collection(db, "adoration_registrations"), {
+            name: nameInputValue,
+            email: emailInputValue,
+            timestamp: new Date()
+        })
+
+        alert("Registration successfull!")
+        form.reset()
+    } catch (error) {
+        console.error("Error adding document", error)
+        alert("Something went wrong, try again!")
+    }
 })
